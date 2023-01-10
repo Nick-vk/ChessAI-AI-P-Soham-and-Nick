024840a60b55b14f9dadfa13edea7ab56647bd7e
Nick-vk <n.a.vankoverden@student.utwente.nl>
2023-01-10 16:42:32 +0100

    evaluate position

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import chess
+import chess.polygot
 
 board = chess.Board()
 
@@ -58,3 +59,70 @@ kings_table = [
     -30, -40, -40, -50, -50, -40, -40, -30,
     -30, -40, -40, -50, -50, -40, -40, -30,
     -30, -40, -40, -50, -50, -40, -40, -30]
+
+
+# update
+def __init__():
+    chess_algorithm()
+
+
+# check end state of game
+def chess_algorithm():
+    if board.is_checkmate():
+        if board.turn:
+            return -9999
+        else:
+            return 9999
+    if board.is_stalemate():
+        return 0
+    if board.is_insufficient_material():
+        return 0
+
+
+# calculate_pieces
+    wp = len(board.pieces(chess.PAWN, chess.WHITE))
+    bp = len(board.pieces(chess.PAWN, chess.BLACK))
+    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+    wr = len(board.pieces(chess.ROOK, chess.WHITE))
+    br = len(board.pieces(chess.ROOK, chess.BLACK))
+    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+
+# calculate_scores
+    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+    pawnsq = sum([pawns_table[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+    pawnsq = pawnsq + sum([-pawns_table[chess.square_mirror(i)]
+                           for i in board.pieces(chess.PAWN, chess.BLACK)])
+    knightsq = sum([knights_table[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+    knightsq = knightsq + sum([-knights_table[chess.square_mirror(i)]
+                               for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+    bishopsq = sum([bishops_table[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+    bishopsq = bishopsq + sum([-bishops_table[chess.square_mirror(i)]
+                               for i in board.pieces(chess.BISHOP, chess.BLACK)])
+    rooksq = sum([rooks_table[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
+    rooksq = rooksq + sum([-rooks_table[chess.square_mirror(i)]
+                           for i in board.pieces(chess.ROOK, chess.BLACK)])
+    queensq = sum([queens_table[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
+    queensq = queensq + sum([-queens_table[chess.square_mirror(i)]
+                             for i in board.pieces(chess.QUEEN, chess.BLACK)])
+    kingsq = sum([kings_table[i] for i in board.pieces(chess.KING, chess.WHITE)])
+    kingsq = kingsq + sum([-kings_table[chess.square_mirror(i)]
+                           for i in board.pieces(chess.KING, chess.BLACK)])
+
+
+# evaluate position
+    eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
+    if board.turn:
+        return eval
+    else:
+        return -eval
+
+
+# openings we have to download this
+    try:
+        move = chess.polyglot.MemoryMappedReader("D:/Users/Carstens/books/human.bin").weighted_choice(board).move
+        return move
