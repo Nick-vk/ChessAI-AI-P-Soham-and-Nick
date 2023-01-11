05d9af508ecea2e045521280bd4f2473dab21383
Soham <sohamnanwani007@gmail.com>
2023-01-11 17:41:12 +0100

    new heuristics

--- new.py
+++ new.py
@@ -1,6 +1,7 @@
 import chess
 import random
 
+
 def make_move(board):
     """
     Makes a move for the chess engine.
@@ -20,8 +21,33 @@ def make_move(board):
             if chess.square_name(move.to_square) in center_squares:
                 return move
 
-    # heuristic 3: move a random piece
+    # heuristic 3: Avoid moving into check
+    for move in legal_moves:
+        board.push(move)
+        if board.is_check():
+            board.pop()
+            continue
+        board.pop()
+
+    # heuristic 4:  Prioritize development of minor pieces: Prioritize moves that develop minor pieces (control more
+    # squares on the board)
+    for move in legal_moves:
+        if board.piece_at(move.from_square).symbol() in ["N", "B"]:
+            return move
+
+    # heursistic 5: Control the center: Prioritize moves that increase control over the center squares
+    center_squares = ["d4", "d5", "e4", "e5"]
+    for move in legal_moves:
+        if chess.square_name(move.to_square) in center_squares:
+            return move
+
+    # heursictic 6: Prioritize moves that improves the king safety by castling or moving the king to a safer place.
+    if board.is_castling(move) or board.piece_at(move.from_square).symbol() == "K":
+        return move
+
+    # heuristic 7: move a random piece
     return random.choice(legal_moves)
+
 
 # create a chess board
 board = chess.Board()
