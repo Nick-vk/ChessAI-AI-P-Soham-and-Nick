9ed1bfed2040c5605d666d2e07bb2868c2298333
Nick-vk <n.a.vankoverden@student.utwente.nl>
2023-01-18 11:15:23 +0100

    removed comment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import chess
 board = chess.Board()
-
-# this does not work like that
-# engine = chess.engine.SimpleEngine.popen_uci(["python.exe", "D:/Program Files/JetBrains/ChessAI-AI-P-Soham-and-Nick/SimpleEngine.py"])
 from new import ChessEngine
 engine = ChessEngine
 
