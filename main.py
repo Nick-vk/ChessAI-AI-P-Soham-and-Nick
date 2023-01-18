1e41a17bb1af49437941172058be7424a391d24b
Soham <sohamnanwani007@gmail.com>
2023-01-18 11:13:53 +0100

    main clean

--- main.py
+++ main.py
@@ -7,17 +7,6 @@ from new import ChessEngine
 engine = ChessEngine
 
 
-while not board.is_game_over():
-    if board.turn:
-        print(board)
-        print("Enter your move:")
-        move = input()
-        try:
-            board.push_uci(move)
-        except:
-            print("Invalid move. Please enter a valid move.")
-    else:
-        result = engine.make_move
-        board.push(result)
+engine.play()
 engine.quit()
 
