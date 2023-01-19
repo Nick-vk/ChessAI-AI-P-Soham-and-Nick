8050171a342700e960d13711239044c28b588989
Nick-vk <n.a.vankoverden@student.utwente.nl>
2023-01-19 11:35:08 +0100

    simple engine class

--- SimpleEngine.py
+++ SimpleEngine.py
@@ -2,8 +2,8 @@ import chess
 import chess.polyglot
 import chess.svg
 import chess.pgn
+# from IPython.display import SVG
 
-from main import board
 
 # heuristic tables per piece
 pawns_table = [
@@ -63,117 +63,135 @@ kings_table = [
     -30, -40, -40, -50, -50, -40, -40, -30]
 
 
-# check end state of game
-def evaluate_board():
-    if board.is_checkmate():
+class SimpleEngine:
+    def __init__(self):
+        self.board = chess.Board()
+
+    # check end state of game
+    def evaluate_board(self):
+        if board.is_checkmate():
+            if board.turn:
+                return -9999
+            else:
+                return 9999
+        if board.is_stalemate():
+            return 0
+        if board.is_insufficient_material():
+            return 0
+
+    # calculate_pieces
+        wp = len(board.pieces(chess.PAWN, chess.WHITE))
+        bp = len(board.pieces(chess.PAWN, chess.BLACK))
+        wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
+        bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
+        wb = len(board.pieces(chess.BISHOP, chess.WHITE))
+        bb = len(board.pieces(chess.BISHOP, chess.BLACK))
+        wr = len(board.pieces(chess.ROOK, chess.WHITE))
+        br = len(board.pieces(chess.ROOK, chess.BLACK))
+        wq = len(board.pieces(chess.QUEEN, chess.WHITE))
+        bq = len(board.pieces(chess.QUEEN, chess.BLACK))
+
+    # calculate_scores
+        material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
+
+        pawnsq = sum([pawns_table[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
+        pawnsq = pawnsq + sum([-pawns_table[chess.square_mirror(i)]
+                               for i in board.pieces(chess.PAWN, chess.BLACK)])
+        knightsq = sum([knights_table[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
+        knightsq = knightsq + sum([-knights_table[chess.square_mirror(i)]
+                                   for i in board.pieces(chess.KNIGHT, chess.BLACK)])
+        bishopsq = sum([bishops_table[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
+        bishopsq = bishopsq + sum([-bishops_table[chess.square_mirror(i)]
+                                   for i in board.pieces(chess.BISHOP, chess.BLACK)])
+        rooksq = sum([rooks_table[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
+        rooksq = rooksq + sum([-rooks_table[chess.square_mirror(i)]
+                               for i in board.pieces(chess.ROOK, chess.BLACK)])
+        queensq = sum([queens_table[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
+        queensq = queensq + sum([-queens_table[chess.square_mirror(i)]
+                                 for i in board.pieces(chess.QUEEN, chess.BLACK)])
+        kingsq = sum([kings_table[i] for i in board.pieces(chess.KING, chess.WHITE)])
+        kingsq = kingsq + sum([-kings_table[chess.square_mirror(i)]
+                               for i in board.pieces(chess.KING, chess.BLACK)])
+
+    # evaluate position
+        eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
         if board.turn:
-            return -9999
+            return eval
         else:
-            return 9999
-    if board.is_stalemate():
-        return 0
-    if board.is_insufficient_material():
-        return 0
+            return -eval
 
-
-# calculate_pieces
-    wp = len(board.pieces(chess.PAWN, chess.WHITE))
-    bp = len(board.pieces(chess.PAWN, chess.BLACK))
-    wn = len(board.pieces(chess.KNIGHT, chess.WHITE))
-    bn = len(board.pieces(chess.KNIGHT, chess.BLACK))
-    wb = len(board.pieces(chess.BISHOP, chess.WHITE))
-    bb = len(board.pieces(chess.BISHOP, chess.BLACK))
-    wr = len(board.pieces(chess.ROOK, chess.WHITE))
-    br = len(board.pieces(chess.ROOK, chess.BLACK))
-    wq = len(board.pieces(chess.QUEEN, chess.WHITE))
-    bq = len(board.pieces(chess.QUEEN, chess.BLACK))
-
-
-# calculate_scores
-    material = 100 * (wp - bp) + 320 * (wn - bn) + 330 * (wb - bb) + 500 * (wr - br) + 900 * (wq - bq)
-
-    pawnsq = sum([pawns_table[i] for i in board.pieces(chess.PAWN, chess.WHITE)])
-    pawnsq = pawnsq + sum([-pawns_table[chess.square_mirror(i)]
-                           for i in board.pieces(chess.PAWN, chess.BLACK)])
-    knightsq = sum([knights_table[i] for i in board.pieces(chess.KNIGHT, chess.WHITE)])
-    knightsq = knightsq + sum([-knights_table[chess.square_mirror(i)]
-                               for i in board.pieces(chess.KNIGHT, chess.BLACK)])
-    bishopsq = sum([bishops_table[i] for i in board.pieces(chess.BISHOP, chess.WHITE)])
-    bishopsq = bishopsq + sum([-bishops_table[chess.square_mirror(i)]
-                               for i in board.pieces(chess.BISHOP, chess.BLACK)])
-    rooksq = sum([rooks_table[i] for i in board.pieces(chess.ROOK, chess.WHITE)])
-    rooksq = rooksq + sum([-rooks_table[chess.square_mirror(i)]
-                           for i in board.pieces(chess.ROOK, chess.BLACK)])
-    queensq = sum([queens_table[i] for i in board.pieces(chess.QUEEN, chess.WHITE)])
-    queensq = queensq + sum([-queens_table[chess.square_mirror(i)]
-                             for i in board.pieces(chess.QUEEN, chess.BLACK)])
-    kingsq = sum([kings_table[i] for i in board.pieces(chess.KING, chess.WHITE)])
-    kingsq = kingsq + sum([-kings_table[chess.square_mirror(i)]
-                           for i in board.pieces(chess.KING, chess.BLACK)])
-
-
-# evaluate position
-    eval = material + pawnsq + knightsq + bishopsq + rooksq + queensq + kingsq
-    if board.turn:
-        return eval
-    else:
-        return -eval
-
-
-def alphabeta(alpha, beta, depthleft):
-    bestscore = -9999
-    if (depthleft == 0):
-        return quiesce(alpha, beta)
-    for move in board.legal_moves:
-        board.push(move)
-        score = -alphabeta(-beta, -alpha, depthleft - 1)
-        board.pop()
-        if (score >= beta):
-            return score
-        if (score > bestscore):
-            bestscore = score
-        if (score > alpha):
-            alpha = score
-    return bestscore
-
-
-def quiesce(alpha, beta):
-    stand_pat = evaluate_board()
-    if (stand_pat >= beta):
-        return beta
-    if (alpha < stand_pat):
-        alpha = stand_pat
-    for move in board.legal_moves:
-        if board.is_capture(move):
-            board.push(move)
-            score = -quiesce(-beta, -alpha)
-            board.pop()
-
-        if (score >= beta):
-            return beta
-        if (score > alpha):
-            alpha = score
-    return alpha
-
-
-def try_moves(depth):
-    # openings we have to download this
-    try:
-        # change to your file location
-        move = chess.polyglot.MemoryMappedReader("D:/Program Files/JetBrains/PythonBooks/human.bin").weighted_choice(board).move
-        return move
-    except:
-        bestMove = chess.Move.null()
-        bestValue = -99999
-        alpha = -100000
-        beta = 100000
+    def alphabeta(self, alpha, beta, depthleft):
+        bestscore = -9999
+        if (depthleft == 0):
+            return quiesce(alpha, beta)
         for move in board.legal_moves:
             board.push(move)
-            boardValue = -alphabeta(-beta, -alpha, depth - 1)
-            if boardValue > bestValue:
-                bestValue = boardValue
-                bestMove = move
-            if (boardValue > alpha):
-                alpha = boardValue
+            score = -alphabeta(-beta, -alpha, depthleft - 1)
             board.pop()
-        return bestMove
+            if (score >= beta):
+                return score
+            if (score > bestscore):
+                bestscore = score
+            if (score > alpha):
+                alpha = score
+        return bestscore
+
+    def quiesce(self, alpha, beta):
+        stand_pat = evaluate_board()
+        if (stand_pat >= beta):
+            return beta
+        if (alpha < stand_pat):
+            alpha = stand_pat
+        for move in board.legal_moves:
+            if board.is_capture(move):
+                board.push(move)
+                score = -quiesce(-beta, -alpha)
+                board.pop()
+
+            if (score >= beta):
+                return beta
+            if (score > alpha):
+                alpha = score
+        return alpha
+
+    def try_moves(self, depth):
+        # openings we have to download this
+        try:
+            # change to your file location
+            move = chess.polyglot.MemoryMappedReader("D:/Program Files/JetBrains/PythonBooks/human.bin").weighted_choice(board).move
+            return move
+        except:
+            bestMove = chess.Move.null()
+            bestValue = -99999
+            alpha = -100000
+            beta = 100000
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                boardValue = -alphabeta(-beta, -alpha, depth - 1)
+                if boardValue > bestValue:
+                    bestValue = boardValue
+                    bestMove = move
+                if (boardValue > alpha):
+                    alpha = boardValue
+                board.pop()
+            return bestMove
+
+    def play(self):
+        while not self.board.is_game_over():
+            if self.board.turn == chess.WHITE:
+                move = input("Please enter your move: ")
+                try:
+                    self.board.push_san(move)
+                except ValueError:
+                    print("Invalid mode, please enter a valid move.")
+            else:
+                move = self.try_moves(depth=1)
+                print("Engine move: ", move)
+                self.board.push(move)
+
+            # print board after each move and result after the game ends
+            chess.svg.board(self.board, size=350)
+        chess.svg.result(self.board, size=350)
+
+
+SimpleEngine().play()
