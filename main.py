535b585758e3f6f6d65db64cdc2a3257ff3a5dc9
Nick <rumo667@gmail.com>
2023-01-24 20:25:33 +0100

    update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 # https://en.wikipedia.org/wiki/Negamax
 # https://www.freecodecamp.org/news/simple-chess-ai-step-by-step-1d55a9266977/
 # https://medium.com/dscvitpune/lets-create-a-chess-ai-8542a12afef
+
 from OptimizedEngine import SimpleEngine
 
 engine = SimpleEngine()
